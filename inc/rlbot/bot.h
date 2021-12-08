25d67d599d92c035fcbbbb8b6129496e52a27eb0
Sam Mish <samuelpmish@gmail.com>
2021-12-07 19:42:05 -0800

    add websocketpp/asio, start implementing tmcp 1.0

--- inc/rlbot/bot.h
+++ inc/rlbot/bot.h
@@ -3,7 +3,13 @@
 #include "rlbot/packets.h"
 #include "rlbot/rlbot_generated.h"
 
+#include "tmcp.hpp"
+#include <websocketpp/server.hpp>
+#include <websocketpp/config/asio_no_tls.hpp>
+
 #include <string>
+
+using server = websocketpp::server<websocketpp::config::asio>;
 
 namespace rlbot {
 class Bot {
@@ -14,14 +20,16 @@ public:
   int index;
   int team;
   std::string name;
+  server matchcomms_connection;
 
-  Bot(int index, int team, std::string name);
+  Bot(int index, int team, std::string name, std::string uri = std::string());
   virtual ~Bot() {}
   virtual Controller GetOutput(GameTickPacket gametickpacket) = 0;
 
   BallPrediction GetBallPrediction();
   FieldInfo GetFieldInfo();
   MatchInfo GetMatchInfo();
+  MatchComms::Packet GetMatchComms();
 
   void SendQuickChat(rlbot::flat::QuickChatSelection message, bool teamOnly);
   QuickChatMessages ReceiveQuickChat();
